9905f25e4f15e85cb4c784cadc10ca2e37dcaa8f
SantiagoIg06 <hibanez1806@gmail.com>
2024-07-29 16:05:01 -0500

    Portfolio V1

--- NavBarGral/navBar.js
+++ NavBarGral/navBar.js
@@ -4,9 +4,9 @@ window.onload = function() {
     if (nav) {
         nav.innerHTML = `
             <div class="nav-list">
-                <li><a href="/index.html">Inicio</a></li>
-                <li><a href="/Calculadora/index.html">Calculadora</a></li>
-                <li><a href="/NavBar/index.html">NavBar</a></li>
+                <li><a href="../index.html">Inicio</a></li>
+                <li><a href="../Calculadora/index.html">Calculadora</a></li>
+                <li><a href="../NavBar/index.html">NavBar</a></li>
                 <li><a href="#">C</a></li>
                 <li><a href="#">C</a></li>
             </div>
