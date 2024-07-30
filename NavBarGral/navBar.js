5bba90ae580710b0a501e540a96414b8ab20c853
SantiagoIg06 <hibanez1806@gmail.com>
2024-07-30 15:51:42 -0500

    Carpeta responsive agregada

--- NavBarGral/navBar.js
+++ NavBarGral/navBar.js
@@ -1,3 +1,11 @@
+let head = document.querySelector("head");
+head.innerHTML += `
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100..900&family=Ubuntu:wght@300&display=swap" rel="stylesheet"></link>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+`;
+
 window.onload = function() {
     let nav = document.getElementById("NavBarGral");
 
@@ -8,10 +16,12 @@ window.onload = function() {
                 <li><a href="../Calculadora/">Calculadora</a></li>
                 <li><a href="../NavBar/">Barra de navegacion</a></li>
                 <li><a href="../Mode/">Modo claro y oscuro</a></li>
-                <li><a href="#">C</a></li>
+                <li><a href="../Responsive/">Menu Responsive</a></li>
             </div>
         `;
     } else {
         console.error("El elemento con id 'NavBarGral' no se encontró.");
     }
 };
+
+
