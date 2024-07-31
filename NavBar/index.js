9aede405272ce57f782e3d344b48d928f54122c3
SantiagoIg06 <hibanez1806@gmail.com>
2024-07-31 15:44:50 -0500

    Portafolio V2

--- NavBar/index.js
+++ NavBar/index.js
@@ -1,10 +1,23 @@
 // V2
+const navItems = ["csaId","pflId", "msjId","cmrId","ajsId"];
+
 document.getElementById('menuButton').addEventListener('click', function() {
     document.querySelector('.navBar').classList.add('expanded');
+    navItems.forEach(id => {
+        const element = document.getElementById(id);
+        element.classList.add("expanded");
+        element.classList.remove("notExpanded"); 
+    });
     document.getElementById('menuButton').classList.remove('btn-menu-img');
 });
 
 document.getElementById('closeButton').addEventListener('click', function() {
     document.querySelector('.navBar').classList.remove('expanded');
+    navItems.forEach(id => {
+        const element = document.getElementById(id);
+        element.classList.add("notExpanded");
+        element.classList.remove("expanded"); 
+    });
     document.getElementById('menuButton').classList.add('btn-menu-img');
 });
+
